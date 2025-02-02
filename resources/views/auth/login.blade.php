0939527346775eb5f946b01a74b39af6c2dbd5be
ManHarun <wayanwidi221@gmail.com>
2025-02-02 22:34:45 +0800

    fix: page login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,9 +44,9 @@
                 <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
             </div>
             <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
-            <div class="text-sm font-medium text-gray-900 dark:text-white">
+            {{-- <div class="text-sm font-medium text-gray-900 dark:text-white">
               Daftar dari? <a href="/sigin" class="text-blue-600 hover:underline dark:text-blue-500">Create account</a>
-            </div>
+            </div> --}}
         </form>
       </div>
     </div>
